cd08aec729f2e403bd869ffdd470ed8de9b83c3b
Luis Revilla <luisrevillameza@gmail.com>
2020-04-25 16:45:39 -0400

    basic deps

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
 const port = 8080; //
+
+
+app.use(cors);
+
+app.use(express.json());
 
 // define a route handler for the default home page
 app.get('/', (req, res) => {
