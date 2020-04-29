033a80cc227320cc73ab74f49fe9b66b085fe237
Luis Revilla <luisrevillameza@gmail.com>
2020-04-29 19:49:35 -0400

    Add db connection

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import http from 'http';
 import express from 'express';
 import { applyMiddleware, applyRoutes } from './utils';
+import { createConnection } from 'typeorm';
 import middleware from './middleware';
 import errorHandlers from './middleware/errorHandlers';
 import routes from './routes';
@@ -15,12 +16,14 @@ process.on('unhandledRejection', (e) => {
   process.exit(1);
 });
 
-const router = express();
-applyMiddleware(middleware, router);
-applyRoutes(routes, router);
-applyMiddleware(errorHandlers, router);
 
-const { PORT = 3000 } = process.env;
-const server = http.createServer(router);
+createConnection().then(() => {
+  const router = express();
+  applyMiddleware(middleware, router);
+  applyRoutes(routes, router);
+  applyMiddleware(errorHandlers, router);
 
-server.listen(PORT, () => console.log(`Server is running http://localhost:${PORT}...`));
+  const { PORT = 3000 } = process.env;
+  const server = http.createServer(router);
+  server.listen(PORT, () => console.log(`Server is running http://localhost:${PORT}...`));
+});
